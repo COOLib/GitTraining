270828ad6d1111d704d1bbb6bea27903e6e33a7e
Ruslan Kulibabin <Ruslan_Kulibabin@epam.com>
2019-01-24 16:33:52 +0200

    First commit

---
+++ demo/src/main/java/com/example/demo/NewService.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewService {
+
+    private Long longField;
+    private String stringField;
+    private Boolean aBooleanField;
+}
